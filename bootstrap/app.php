73ddd0cb95ccbabff4e0df49210f9d5f7dd16e84
baybilon <baybilon_iqbal@hotmail.com>
2026-02-13 00:48:15 +0700

    Change config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED & ~E_NOTICE);
+ini_set('display_errors', '0');
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
