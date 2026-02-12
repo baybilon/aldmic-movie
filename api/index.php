cdf53605a00aaf4d51144480c20f45c701b88e8b
baybilon <baybilon_iqbal@hotmail.com>
2026-02-12 23:55:14 +0700

    Change config

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+error_reporting(0);
+ini_set('display_errors', '0');
 
+require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../public/index.php';
