ab9aaad474f597a8ab351d9df855064af24305cc
baybilon <baybilon_iqbal@hotmail.com>
2026-02-12 23:48:10 +0700

    Change config

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
-// Mematikan semua peringatan deprecated sebelum memuat Laravel
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
-ini_set('display_errors', '0');
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
 require __DIR__ . '/../public/index.php';
